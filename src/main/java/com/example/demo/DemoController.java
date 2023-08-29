3643b5a4ec53a804396cae8a0eaee134a2e15547
ren2003777 <r.goino@onodera-internet-service.com>
2023-08-29 15:02:12 +0900

    HTMLファイルまたは、Democontrollerに間違いがあると思うがわからない。

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class DemoController {
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model) {
+		int sum = 0;
+		for(int i = 1 ; i <= num ; i++) {
+			sum += i;
+		}
+		model.addAttribute("msg", "sum=" + sum);
+		return "index";
+	}
+}

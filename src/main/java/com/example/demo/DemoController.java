822e4d9d2decafdbf0fe4e12e3034bf8dd9916d9
ren2003777 <r.goino@onodera-internet-service.com>
2023-09-05 10:38:02 +0900

    ビューからのデータ入力

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,21 +1,27 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DemoController {
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num, Model model) {
-		int sum = 0;
-		for(int i = 1 ; i <= num ; i++) {
-			sum += i;
-		}
-		model.addAttribute("msg", "sum=" + sum);
-		return "index";
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "お名前を入力してください");
+		return mav;
 	}
 
-
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(
+			@RequestParam("text1")String str,
+			ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "こんにちは" + str + "さん！");
+		mav.addObject("value", str);
+		return mav;
+	}
 }
